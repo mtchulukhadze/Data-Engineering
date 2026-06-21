d06628f8dfc29ffb2f327e15e96510f0317281e3
mtchulukhadze <mikheili.tchulukhadze.1@btu.edu.ge>
2026-06-21 21:01:07 +0400

    etl

---
+++ Airflow/Extract.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+import requests
+
+url = "https://fakestoreapi.com"
+
+def extract_products():
+    url2 = f"{url}/products"
+    response = requests.get(url2)
+    response.raise_for_status()
+
+    data = response.json()
+
+    data2 = pd.DataFrame(data)
+    return data2
+
+
+def extract_user():
+    url3 = f"{url}/users"
+    response = requests.get(url3)
+    response.raise_for_status()
+
+    data = response.json()
+
+    data2 = pd.DataFrame(data)
+    return data2
